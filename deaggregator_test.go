4243900ad1e5fee0226bcc9aad185102ce54e471
sotaro-kimura <sotaro_kimura@dwango.co.jp>
2018-04-09 16:15:40 +0900

    Fix testcode.

--- deaggregator_test.go
+++ deaggregator_test.go
@@ -3,6 +3,7 @@ package deaggregation
 import (
 	"crypto/md5"
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
@@ -16,10 +17,8 @@ func Test_IsAggregatedRecord_JudgeNonAggregatedRecord(t *testing.T) {
 	targetByteArray := []byte(target)
 
 	actual := IsAggregatedRecord(targetByteArray)
-	if actual == false {
-		t.Log("NotAggregatedRecord judge succeed.")
-	} else {
-		t.Fail()
+	if actual != false {
+		t.Errorf("IsAggregatedRecord(\"%v\") want %v but %v.", target, false, actual)
 	}
 }
 
@@ -34,10 +33,8 @@ func Test_IsAggregatedRecord_JudgeMinimumAggregatedRecord(t *testing.T) {
 	target = append(target, checkSum...)
 
 	actual := IsAggregatedRecord(target)
-	if actual == true {
-		t.Log("MinimumAggregatedRecord judge succeed.")
-	} else {
-		t.Fail()
+	if actual != true {
+		t.Errorf("IsAggregatedRecord(MinimumAggregatedRecord) want %v but %v.", true, actual)
 	}
 }
 
@@ -52,10 +49,8 @@ func Test_IsAggregatedRecord_JudgeFullAggregatedRecord(t *testing.T) {
 	targetBytes = append(targetBytes, checkSum...)
 
 	actual := IsAggregatedRecord(targetBytes)
-	if actual == true {
-		t.Log("MinimumAggregatedRecord judge succeed.")
-	} else {
-		t.Fail()
+	if actual != true {
+		t.Errorf("IsAggregatedRecord(FullAggregatedRecord) want %v but %v.", true, actual)
 	}
 }
 
@@ -68,12 +63,11 @@ func Test_ExtractRecordDatas_MinimumAggregatedRecord(t *testing.T) {
 	targetBytes := append(magicNumber, targetRecord...)
 	targetBytes = append(targetBytes, checkSum...)
 
+	expected := [][]byte{[]byte("MinimumAggregatedRecord")}
 	actual, _ := ExtractRecordDatas(targetBytes)
 
-	if len(actual) == 1 && string(actual[0]) == "MinimumAggregatedRecord" {
-		t.Log("MinimumAggregatedRecord extract succeed.")
-	} else {
-		t.Fail()
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("ExtractRecordDatas(MinimumAggregatedRecord) want %v but %v.", expected, actual)
 	}
 }
 
@@ -86,12 +80,11 @@ func Test_ExtractRecordDatas_FullAggregatedRecord(t *testing.T) {
 	targetBytes := append(magicNumber, targetRecord...)
 	targetBytes = append(targetBytes, checkSum...)
 
+	expected := [][]byte{[]byte("FullAggregatedRecord1"), []byte("FullAggregatedRecord2")}
 	actual, _ := ExtractRecordDatas(targetBytes)
 
-	if len(actual) == 2 && string(actual[0]) == "FullAggregatedRecord1" && string(actual[1]) == "FullAggregatedRecord2" {
-		t.Log("FullAggregatedRecord extract succeed.")
-	} else {
-		t.Fail()
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("ExtractRecordDatas(FullAggregatedRecord) want %v but %v.", expected, actual)
 	}
 }
 
@@ -106,16 +99,29 @@ func Test_Unmarshal_MinimumAggregatedRecord(t *testing.T) {
 
 	actual, _ := Unmarshal(targetBytes)
 
-	if len(actual.GetExplicitHashKeyTable()) == 0 && len(actual.GetPartitionKeyTable()) == 0 && len(actual.GetRecords()) == 1 {
-		t.Log("Record extract succeed.")
-	} else {
-		t.Fail()
+	if len(actual.GetExplicitHashKeyTable()) != 0 {
+		t.Errorf("Unmarshal(MinimumAggregatedRecord)'s ExplicitHashKeyTable want %v results but %v results.", 0, actual.GetExplicitHashKeyTable())
 	}
+	if len(actual.GetPartitionKeyTable()) != 0 {
+		t.Errorf("Unmarshal(MinimumAggregatedRecord)'s PartitionKeyTable want %v results but %v results.", 0, actual.GetPartitionKeyTable())
+	}
+	if len(actual.GetRecords()) != 1 {
+		t.Errorf("Unmarshal(MinimumAggregatedRecord)'s Records want %v records but %v records.", 1, len(actual.GetRecords()))
+	}
+
 	actualRecord := actual.GetRecords()[0]
-	if actualRecord.GetPartitionKeyIndex() == uint64(1111) && actualRecord.GetExplicitHashKeyIndex() == uint64(0) && string(actualRecord.GetData()) == "MinimumAggregatedRecord" && len(actualRecord.GetTags()) == 0 {
-		t.Log("Record check succeed.")
-	} else {
-		t.Fail()
+	if actualRecord.GetPartitionKeyIndex() != uint64(1111) {
+		t.Errorf("PartitionKeyIndex want %v but %v.", uint64(1111), actualRecord.GetPartitionKeyIndex())
+	}
+	if actualRecord.GetExplicitHashKeyIndex() != uint64(0) {
+		t.Errorf("ExplicitHashKeyIndex want %v but %v.", uint64(0), actualRecord.GetExplicitHashKeyIndex())
+	}
+	if len(actualRecord.GetTags()) != 0 {
+		t.Errorf("Tags want %v tags but %v tags.", 0, len(actualRecord.GetTags()))
+	}
+	expected := "MinimumAggregatedRecord"
+	if string(actualRecord.GetData()) != expected {
+		t.Errorf("Data want %v but %v.", expected, string(actualRecord.GetData()))
 	}
 }
 
@@ -130,41 +136,34 @@ func Test_Unmarshal_FullAggregatedRecord(t *testing.T) {
 
 	actual, _ := Unmarshal(targetBytes)
 
-	if len(actual.GetExplicitHashKeyTable()) == 2 && len(actual.GetPartitionKeyTable()) == 2 && len(actual.GetRecords()) == 2 {
-		t.Log("Record extract succeed.")
-	} else {
-		t.Fail()
+	expectedPartitionKeyTable := []string{"Table1", "Table2"}
+	if !reflect.DeepEqual(expectedPartitionKeyTable, actual.GetPartitionKeyTable()) {
+		t.Errorf("Unmarshal(FullAggregatedRecord)'s PartitionKeyTable want %v but %v.", expectedPartitionKeyTable, actual.GetPartitionKeyTable())
 	}
-	if actual.GetPartitionKeyTable()[0] == "Table1" && actual.GetPartitionKeyTable()[1] == "Table2" && actual.GetExplicitHashKeyTable()[0] == "KeyTable1" && actual.GetExplicitHashKeyTable()[1] == "KeyTable2" {
-		t.Log("Parent record check succeed.")
-	} else {
-		t.Fail()
+	expectedExplicitHashKeyTable := []string{"KeyTable1", "KeyTable2"}
+	if !reflect.DeepEqual(expectedExplicitHashKeyTable, actual.GetExplicitHashKeyTable()) {
+		t.Errorf("Unmarshal(FullAggregatedRecord)'s ExplicitHashKeyTable want %v but %v.", expectedExplicitHashKeyTable, actual.GetExplicitHashKeyTable())
+	}
+	if len(actual.GetRecords()) != 2 {
+		t.Errorf("Unmarshal(FullAggregatedRecord)'s Records want %v records but %v records.", 2, len(actual.GetRecords()))
 	}
 
 	actualRecord1 := actual.GetRecords()[0]
 	actualRecord2 := actual.GetRecords()[1]
-	if actualRecord1.GetPartitionKeyIndex() == uint64(1111) && actualRecord1.GetExplicitHashKeyIndex() == uint64(11111111) && string(actualRecord1.GetData()) == "FullAggregatedRecord1" && len(actualRecord1.GetTags()) == 2 {
-		t.Log("Record1 check succeed.")
-	} else {
-		t.Fail()
+	if actualRecord1.GetPartitionKeyIndex() != uint64(1111) || actualRecord1.GetExplicitHashKeyIndex() != uint64(11111111) || string(actualRecord1.GetData()) != "FullAggregatedRecord1" || len(actualRecord1.GetTags()) != 2 {
+		t.Error("Record1 check failed.")
 	}
-	if actualRecord2.GetPartitionKeyIndex() == uint64(2222) && actualRecord2.GetExplicitHashKeyIndex() == uint64(22222222) && string(actualRecord2.GetData()) == "FullAggregatedRecord2" && len(actualRecord2.GetTags()) == 2 {
-		t.Log("Record2 check succeed.")
-	} else {
-		t.Fail()
+	if actualRecord2.GetPartitionKeyIndex() != uint64(2222) || actualRecord2.GetExplicitHashKeyIndex() != uint64(22222222) || string(actualRecord2.GetData()) != "FullAggregatedRecord2" || len(actualRecord2.GetTags()) != 2 {
+		t.Error("Record2 check failed.")
 	}
 
 	actualRecord1Tags := actualRecord1.GetTags()
 	actualRecord2Tags := actualRecord2.GetTags()
-	if actualRecord1Tags[0].GetKey() == "TagKey1" && actualRecord1Tags[0].GetValue() == "TagValue1" && actualRecord1Tags[1].GetKey() == "TagKey2" && actualRecord1Tags[1].GetValue() == "TagValue2" {
-		t.Log("Record1Tag check succeed.")
-	} else {
-		t.Fail()
+	if actualRecord1Tags[0].GetKey() != "TagKey1" || actualRecord1Tags[0].GetValue() != "TagValue1" || actualRecord1Tags[1].GetKey() != "TagKey2" || actualRecord1Tags[1].GetValue() != "TagValue2" {
+		t.Error("Record1Tag check failed.")
 	}
-	if actualRecord2Tags[0].GetKey() == "TagKey2" && actualRecord2Tags[0].GetValue() == "TagValue2" && actualRecord2Tags[1].GetKey() == "TagKey1" && actualRecord2Tags[1].GetValue() == "TagValue1" {
-		t.Log("Record1Tag check succeed.")
-	} else {
-		t.Fail()
+	if actualRecord2Tags[0].GetKey() != "TagKey2" || actualRecord2Tags[0].GetValue() != "TagValue2" || actualRecord2Tags[1].GetKey() != "TagKey1" || actualRecord2Tags[1].GetValue() != "TagValue1" {
+		t.Error("Record2Tag check failed.")
 	}
 }
 
